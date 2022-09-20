cb6d5dd67390830573d7705d32cf1dddc9294d5c
Oleg <popovolegvrn@gmail.com>
2022-09-20 14:01:46 +0300

    Modified program.cs for Task_1

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите первое целое число: ");
+int first_number = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе целое число: ");
+int second_number = Convert.ToInt32 (Console.ReadLine());
+int max_number = first_number;
+if (second_number > first_number)
+{
+    max_number = second_number;
+    Console.Write ("max = ");
+    Console.WriteLine (max_number);
+}
+else if (second_number == max_number)
+{
+    Console.Write ("Вы ввели одинаковые числа :(");
+    Console.Write ("max = ");
+    Console.WriteLine (max_number);
+}
+else
+{
+    Console.Write ("max = ");
+    Console.WriteLine (max_number);
+}
