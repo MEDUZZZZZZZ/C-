5d420129f2260074350146dc3ec34bf13d5589a5
Oleg <popovolegvrn@gmail.com>
2022-09-20 15:14:30 +0300

    Added Task_3 Program.cs

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.WriteLine ("Программа на вход принимает число и выдаёт, является ли число чётным");
+    Console.Write ("Введите целое число: ");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    int parity = number % 2;
+    if (parity == 0)
+    {
+        Console.WriteLine("Число "+number+" четное!");
+    }
+    else
+    {
+        Console.WriteLine("Число "+number+" нечетное!");
+    }
+}
+catch
+{
+    Console.WriteLine("Необходимо было ввести именно целое число!");
+}
