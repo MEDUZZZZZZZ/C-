a5636b3632c81beb5ae9e193938992a69e4ed1cb
Oleg <popovolegvrn@gmail.com>
2022-09-20 14:58:05 +0300

    Modified Task_2

--- Lesson_1/Task_2/Program.cs
+++ Lesson_1/Task_2/Program.cs
@@ -1,37 +1,41 @@
 ﻿Console.WriteLine ("Данная программа принимает на вход три целых числа и находит максимальное");
 Console.Write ("Введите первое целое число: ");
-int first_number = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Введите второе целое число: ");
-int second_number = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Введите третье целое число: ");
-int third_number = Convert.ToInt32 (Console.ReadLine());
-int max_number = first_number;
-if (max_number >= second_number)
-{
-    if (max_number > third_number)
+try
     {
-        Console.Write("Максимальное число: ");
-        Console.WriteLine(max_number);
+        int first_number = Convert.ToInt32 (Console.ReadLine());
+        Console.Write ("Введите второе целое число: ");
+        int second_number = Convert.ToInt32 (Console.ReadLine());
+        Console.Write ("Введите третье целое число: ");
+        int third_number = Convert.ToInt32 (Console.ReadLine());
+        int max_number = first_number;
+        if (max_number >= second_number)
+        {
+            if (max_number > third_number)
+            {
+                Console.Write("Максимальное число: "+max_number);
+            }
+            else
+            {
+                max_number = third_number;
+                Console.Write("Максимальное число: "+max_number);
+            }
+        }
+        else
+        {
+            max_number = second_number;
+            if (max_number >= third_number)
+            {
+                Console.Write("Максимальное число: "+max_number);
+            }
+            else
+            {
+                max_number = third_number;
+                Console.Write("Максимальное число: "+max_number);
+
+            }
+        }
     }
-    else
+    catch
     {
-        max_number = third_number;
-        Console.Write("Максимальное число: ");
-        Console.WriteLine(max_number);
+        Console.WriteLine("Необходимо вводить целые числа");
     }
-}
-else
-{
-    max_number = second_number;
-    if (max_number >= third_number)
-    {
-        Console.Write("Максимальное число: ");
-        Console.WriteLine(max_number);
-    }
-    else
-    {
-        max_number = third_number;
-        Console.Write("Максимальное число: ");
-        Console.WriteLine(max_number);
-    }
-}
