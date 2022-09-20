df2b538ed8210c790d63a3df034d471b01169087
Oleg <popovolegvrn@gmail.com>
2022-09-20 14:51:46 +0300

    Modified Task_1

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,23 +1,30 @@
 ﻿Console.WriteLine ("Данная программа принимает на вход два целых числа и находит максимальное");
 Console.Write ("Введите первое целое число: ");
-int first_number = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Введите второе целое число: ");
-int second_number = Convert.ToInt32 (Console.ReadLine());
-int max_number = first_number;
-if (second_number > first_number)
+try
 {
-    max_number = second_number;
-    Console.Write ("max = ");
-    Console.WriteLine (max_number);
+    int first_number = Convert.ToInt32 (Console.ReadLine());
+    Console.Write ("Введите второе целое число: ");
+    int second_number = Convert.ToInt32 (Console.ReadLine());
+    int max_number = first_number;
+    if (second_number > first_number)
+    {
+        max_number = second_number;
+        Console.Write ("max = ");
+        Console.WriteLine (max_number);
+    }
+    else if (second_number == max_number)
+    {
+        Console.Write ("Вы ввели одинаковые числа :(");
+        Console.Write ("max = ");
+        Console.WriteLine (max_number);
+    }
+    else
+    {
+        Console.Write ("max = ");
+        Console.WriteLine (max_number);
+    }
 }
-else if (second_number == max_number)
+catch
 {
-    Console.Write ("Вы ввели одинаковые числа :(");
-    Console.Write ("max = ");
-    Console.WriteLine (max_number);
-}
-else
-{
-    Console.Write ("max = ");
-    Console.WriteLine (max_number);
+    Console.WriteLine ("Введите именно целое число");
 }
