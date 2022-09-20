002c20192a5218823a22075c399cd2610e78e1e2
Oleg <popovolegvrn@gmail.com>
2022-09-20 15:29:57 +0300

    Added task_4 program.cs

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.WriteLine ("Программа на вход принимает на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N");
+    Console.Write ("Введите целое число: ");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    int pointer = 1;
+    if (pointer > number)
+    {
+        Console.WriteLine ("Вы ввели число меньше единицы");
+    }
+    else
+    {
+        while (pointer <= number)
+        {
+            int parity = pointer % 2;
+            if (parity == 0)
+            {
+                Console.Write(pointer+",");
+            }
+            pointer++;
+        }  
+    }
+}
+catch
+{
+     Console.WriteLine("Необходимо было ввести именно целое число!");
+}
