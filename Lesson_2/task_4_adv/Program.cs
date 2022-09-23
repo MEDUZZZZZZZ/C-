8705980747630b960578bf5b1ce90633c26492e8
Oleg <popovolegvrn@gmail.com>
2022-09-23 15:46:38 +0300

    Added Task_4 dir

---
+++ Lesson_2/task_4_adv/Program.cs
@@ -0,0 +1,34 @@
+﻿void FindDelSecondNum (int number)
+{
+if (number / 10 == 0 ) Console.WriteLine($"В числе {number} нечего удалять"); 
+else if (number / 100 == 0) 
+{
+    int new_digit = number / 10;
+    Console.WriteLine($"Новое число {new_digit}");
+}
+else 
+{
+    int storage = number;
+    double counter = 0;
+    while (number / 100 > 0)
+    {
+        number /= 10;
+        counter++;
+    }
+    double new_first = (number / 10) * Math.Pow (10, counter);
+    double new_last = storage % (Math.Pow(10, counter));
+    new_first += new_last;
+    Console.WriteLine ($"Новое число {new_first}");
+}
+}
+Console.WriteLine("Данная программа находит и удаляет вторую цифру числа");
+Console.Write("Введите число ");
+try
+{
+    int digit_number = Convert.ToInt32 (Console.ReadLine());
+    FindDelSecondNum(digit_number);
+}
+catch
+{
+    Console.WriteLine("Введите число в цифровом формате");
+}
