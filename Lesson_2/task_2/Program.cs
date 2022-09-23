d2cfda8d9d6ef9d4924a244c2b9f9a244fdbef87
Oleg <popovolegvrn@gmail.com>
2022-09-23 14:33:11 +0300

    Added task_2 Dir

---
+++ Lesson_2/task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿void FindThirdNum (int number)
+{
+    if (number / 100 == 0)
+    {
+    Console.WriteLine($"В числе {number} нет третьей цифры");
+    }
+    else
+    {
+        while (number / 1000 > 0)
+        {
+            number /= 10;
+        }
+        int third_num = number % 10;
+        Console.WriteLine($"Третья цифра числа --> {third_num}");  
+   }
+}
+Console.WriteLine("Данная программа находит третью цифру любого числа");
+Console.Write("Введите число ");
+try
+{
+    int digit_number = Convert.ToInt32 (Console.ReadLine());
+    FindThirdNum(digit_number);
+
+}
+catch
+{
+    Console.WriteLine("Введите число в цифровом формате");
+}
