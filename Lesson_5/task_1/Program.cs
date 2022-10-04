569b0b5cfdee4fa5eb3798c0310c9a0b12675b47
Oleg <popovolegvrn@gmail.com>
2022-10-04 15:08:52 +0300

    Added Task_1 into Lesson_5 Dir

---
+++ Lesson_5/task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿int [] ArrayGen(int size)
+    {
+        int [] mas = new int [size];
+        for (int idx = 0;idx < size;idx++)
+            {
+                mas[idx] = new Random().Next(100,1000);
+            }
+        return mas;
+    }
+
+
+void PrintArray(int[] mas1)
+    {
+        for (int i=0; i<mas1.Length;i++)
+            {
+                if (i != mas1.Length - 1) Console.Write($"{mas1[i]}, ");
+                else Console.WriteLine($"{mas1[i]}");
+            }
+    }
+
+
+int GetEven(int[] mas2)
+{
+    int even_count = 0;
+    foreach (int item in mas2)
+        {
+            if (item % 2 != 0) even_count++;  
+        }
+    return even_count;
+}
+
+int [] my_array = ArrayGen(12);
+PrintArray(my_array);
+int count = GetEven(my_array);
+Console.WriteLine($"Колличество четных чисел в массиве равно: {count}!");
